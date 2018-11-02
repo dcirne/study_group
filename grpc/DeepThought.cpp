bbe070698380f6912e65d6054c1ebf4a7067b025
Dalmo Cirne <dalmo@clarifai.com>
2018-11-02 08:34:06 -0400

    Synchronous stream of gRPC requests and responses

--- grpc/DeepThought.cpp
+++ grpc/DeepThought.cpp
@@ -13,6 +13,28 @@ class DeepThought final : public hitchhiker::Computer::Service {
         answer->set_text("42");
         return grpc::Status::OK;
     }
+
+    grpc::Status Inquires(grpc::ServerContext *context, grpc::ServerReaderWriter<hitchhiker::Answer, hitchhiker::Question> *stream) override {
+        hitchhiker::Question question;
+        while (stream->Read(&question)) {
+            hitchhiker::Answer answer;
+            std::string answerText;
+
+            std::cout << question.text() << std::endl;
+
+            if (question.text() == "What is answer to the ultimate question of life, the universe, and everything?") {
+                answerText = "The answer is: 42";
+            } else if (question.text() == "How many roads must a man walk down?") {
+                answerText = "At least one.";
+            } else {
+                answerText = "Hmmm, let me think. Come back in a thousand years.";
+            }
+
+            answer.set_text(answerText);
+            stream->Write(answer);
+        }
+        return grpc::Status::OK;
+    }
 };
 
 void runDeepThought(const std::string ip, const std::string port) {
--- grpc/Magrathean.cpp
+++ grpc/Magrathean.cpp
@@ -5,13 +5,14 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 class Magrathean final {
     std::unique_ptr<hitchhiker::Computer::Stub> computerStub;
 
   public:
     Magrathean(std::shared_ptr<grpc::Channel> channel) : computerStub(hitchhiker::Computer::NewStub(channel)) {}
-    
+
     std::string askQuestion() {
         grpc::ClientContext clientContext;
         hitchhiker::Question question;
@@ -28,15 +29,62 @@ class Magrathean final {
             return "Deep Thought is not available.";
         }
     }
-};
 
+    std::vector<hitchhiker::Answer> askQuestions(const std::vector<hitchhiker::Question> questions) {
+        grpc::ClientContext clientContext;
+        std::vector<hitchhiker::Answer> answers;
+        answers.reserve(questions.size());
+        
+        std::unique_ptr<grpc::ClientReaderWriter<hitchhiker::Question, hitchhiker::Answer>> stream(computerStub->Inquires(&clientContext));
+        // auto stream = std::make_unique<grpc::ClientReaderWriter<hitchhiker::Question, hitchhiker::Answer>>(computerStub->Inquires(&clientContext));
+
+        for (const auto &question : questions) {
+            stream->Write(question);
+        }
+        stream->WritesDone();
+
+        hitchhiker::Answer answer;
+        while (stream->Read(&answer)) {
+            answers.push_back(answer);
+        }
+
+        grpc::Status status = stream->Finish();
+        if (status.ok()) {
+            return answers;
+        } else {
+            std::cout << "gRPC error. Code: " << status.error_code() << ", message: " << status.error_message() << std::endl;
+            return std::vector<hitchhiker::Answer>();
+        }
+    }
+};
 
 int main(int argc, char **argv) {
     std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel("localhost:4242", grpc::InsecureChannelCredentials());
     Magrathean magrathean(channel);
 
+    // Asks one question
     std::string theAnswer = magrathean.askQuestion();
     std::cout << theAnswer << std::endl;
+
+    // Asks many questions
+    hitchhiker::Question q1;
+    q1.set_text("What is answer to the ultimate question of life, the universe, and everything?");
+
+    hitchhiker::Question q2;
+    q2.set_text("How many roads must a man walk down?");
+
+    hitchhiker::Question q3;
+    q3.set_text("Do you like Volgon poetry?");
+
+    std::vector<hitchhiker::Question> questions;
+    questions.push_back(q1);
+    questions.push_back(q2);
+    questions.push_back(q3);
+
+    auto answers = magrathean.askQuestions(questions);
+    for (const auto &answer : answers) {
+        std::cout << answer.text() << std::endl;
+    }
 
     return 0;
 }
