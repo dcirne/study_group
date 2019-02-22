73ec06bb0976a6de742e8d4073d92071f7df4cfc
Dalmo Cirne <dalmo@clarifai.com>
2019-02-22 14:24:45 -0500

    Glade - User Interface Designer

---
+++ glade/main.c
@@ -0,0 +1,40 @@
+// Copyright 2019 Dalmo Cirne
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+    GtkBuilder *builder;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "clarent.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "clarent_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
+
+void destroyWindow() {
+    printf("Destroy window\n");
+    gtk_main_quit();
+}
